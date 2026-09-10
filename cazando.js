a17b9c87a45e1c65032fe9cfc11e4584c03c7973
monteross718-droid <monteross718@gmail.com>
2026-09-10 18:37:04 -0500

    parte 2 agregada

---
+++ cazando.js
@@ -0,0 +1,7 @@
+let canvas=document.getElementById("areaJuego");
+let ctx=canvas.getContext("2d");
+
+function graficarGato(){
+    graficarRectangulo(gatoX,gatoY,ANCHO_GATO,ALTO_GATO,"orange")
+}
+
